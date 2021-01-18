030c80c6fae507f964dea56f541d0dbc573e8b5b
hellfire <hellfire@gmail.com>
2021-01-18 22:06:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+var clock = +prompt('Который час?');
+switch(clock) {
+    case 0: 
+    alert('Уже полночь, ты ещё не спишь, задрот?');
+    break;
+    case 1: 
+    alert('Уже час ночи, ты ещё не спишь, задрот?');
+    break;
+    case 2: 
+    alert('Уже два часа ночи, ты ещё не спишь, задрот?');
+    break;
+    case 3: 
+    alert('Уже три часа ночи, ты ещё не спишь, задрот?');
+    break;
+    case 4: 
+    alert('Уже три часа ночи, ты ещё не спишь, задрот?');
+    break;
+    case 5: 
+    alert('Пять часов утра');
+    break;
+    case 6: 
+    alert('Шесть часов утра');
+    break;
+    case 7: 
+    alert('Семь часов утра');
+    break;
+    case 8: 
+    alert('Восемь часов утра');
+    break;
+    case 9: 
+    alert('Девять часов утра');
+    break;
+    case 10: 
+    alert('Десять часов утра');
+    break;
+    case 11: 
+    alert('Одиннадцать часов утра');
+    break;
+    case 12: 
+    alert('Полдень, Ержан вставай!');
+    break;
+    case 13: 
+    alert('Час дня');
+    break;
+    case 14: 
+    alert('Два часа дня');
+    break;
+    case 15: 
+    alert('Три часа дня');
+    break;
+    case 16: 
+    alert('Четыре часа дня');
+    break;
+    case 17: 
+    alert('Пять часов вечера');
+    break;
+    case 18: 
+    alert('Шесть часов вечера');
+    break;
+    case 19: 
+    alert('Семь часов вечера');
+    break;
+    case 20: 
+    alert('Восемь часов вечера');
+    break;
+    case 21: 
+    alert('Девять часов ночи');
+    break;
+    case 22: 
+    alert('Десять часов ночи');
+    break;
+    case 23: 
+    alert('Одиннадцать часов ночи');
+    break;
+    case 24:
+    alert('Уже полночь, ты ещё не спишь, задрот?')
+    break;
+    default:
+    alert('Не дури!')
+    break;
+}
